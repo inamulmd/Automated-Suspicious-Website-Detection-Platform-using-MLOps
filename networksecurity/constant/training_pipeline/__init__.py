2b521c0a35300ab41be95f783649371098bc307f
inamulmd <mdinamulh04@gmail.com>
2026-01-29 19:15:27 +0530

    Data_Transformation

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,6 +14,7 @@ FILE_NAME: str ="phisingData.csv"
 
 TRAIN_FILE_NAME:str ="train.csv"
 TEST_FILE_NAME:str ="test.csv"
+PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
 
 SCHEMA_FILE_PATH =os.path.join("data_schema", "schema.yaml")
 
@@ -38,3 +39,19 @@ DATA_VALIDATION_VALID_DIR: str ="validated"
 DATA_VALIDATION_INVALID_DIR:str ="invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str ="drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str ="report.yaml"
+
+
+"""
+Data transformation realted constant start with Data_tranformation Var Name
+"""
+
+DATA_TRANSFORMATION_DIR_NAME:str ="data_transformaion"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str ="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str ="transformed_object"
+
+## knn imputer to replce nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict ={
+    "missing_values":np.nan,
+    "n_neighbors":3,
+    "weights": "uniform",
+}
