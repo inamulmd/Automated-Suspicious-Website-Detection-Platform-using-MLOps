643fcfb2c942edcfe2645d8b0167a260d387b28d
inamulmd <mdinamulh04@gmail.com>
2026-01-30 08:34:51 +0530

    Model Trainer done

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -18,6 +18,9 @@ PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
 
 SCHEMA_FILE_PATH =os.path.join("data_schema", "schema.yaml")
 
+MODEL_FILE_NAME = "model.pkl"
+SAVED_MODEL_DIR=os.path.join("saved_models")
+
 """
 Data Ingestion related constant start with Data_ingestion Var Name
 """
@@ -49,9 +52,20 @@ DATA_TRANSFORMATION_DIR_NAME:str ="data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str ="transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str ="transformed_object"
 
+DATA_TRANFORMED_TRAIN_FILE_PATH:str ="train.npy"
+DATA_TRANSFORED_TEST_FILE_PATH:str ="test.npy"
+
 ## knn imputer to replce nan values
 DATA_TRANSFORMATION_IMPUTER_PARAMS: dict ={
     "missing_values":np.nan,
     "n_neighbors":3,
     "weights": "uniform",
 }
+
+
+
+MODEL_TRAINER_DIR_NAME:str ="model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str="trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME:str="model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float =0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD:float =0.5
