b60389e732e0d2235592c59a6e1c08d46da54ef9
inamulmd <mdinamulh04@gmail.com>
2026-01-30 09:02:07 +0530

    error updated

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -93,7 +93,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
              gs=GridSearchCV(model,para,cv=3)
              gs.fit(X_train,y_train)
 
-             model.set_paraams(**gs.best_params)
+             model.set_params(**gs.best_params_)
              model.fit(X_train,y_train)
 
              y_train_pred = model.predict(X_train)
