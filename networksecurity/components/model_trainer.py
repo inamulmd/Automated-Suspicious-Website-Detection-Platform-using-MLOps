17d2c2ca13f0ca8e621f438dde6f54c444147180
inamulmd <mdinamulh04@gmail.com>
2026-02-01 16:17:30 +0530

    update

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -128,6 +128,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
+        
+        save_object("final_model/model.pkl", best_model)
 
         ## Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(
